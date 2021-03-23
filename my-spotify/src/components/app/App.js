514044492509d4356f0cd5e5a85df67beaa5a993
Johan <johan.le-nagard@epitech.eu>
2021-03-23 21:29:18 +0100

    improve login visibility on the welcome page

--- my-spotify/src/components/app/App.js
+++ my-spotify/src/components/app/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { storage } from '../../redux/store';
 import { Provider } from 'react-redux';
 import Root from '../../router';
+import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
+import theme from '../../theme';
 
 const App = () => {
   return (
     <Provider store={storage}>
-      <Root/>
+      <CssBaseline/>
+      <MuiThemeProvider theme={theme}>
+        <Root/>
+      </MuiThemeProvider>
     </Provider>
   );
 }
--- my-spotify/src/components/app/Welcome.js
+++ my-spotify/src/components/app/Welcome.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from '../../redux/actions/counterActions';
 import Logo from "../../resources/spotify_logo_white.png"
 import Pic1 from '../../resources/pochette1.png'
 import Pic2 from '../../resources/pochette2.jpg'
 import Pic3 from '../../resources/pochette3.jpg'
 import { signIn } from '../../redux/services/services';
 import { getTokenFromURL } from '../../redux/services/services';
-import { saveAccessToken } from '../../redux/actions/userCredentialAction';
+import { saveAccessToken, deleteAccessToken } from '../../redux/actions/userCredentialAction';
 import { useHistory } from "react-router-dom";
+import { AccountCircle } from '@material-ui/icons';
 
 const Welcome = () => {
 
     const history = useHistory();
-    const counter = useSelector(state => state.counterReducers);
     const dispatch = useDispatch();
     let access_token = getTokenFromURL().access_token;
-    
+
     dispatch(saveAccessToken(access_token));
     access_token = useSelector(state => state.userCredentialsReducer);  
   
+    const _signOut = () => {
+      dispatch(deleteAccessToken());
+      history.push("/");
+    }
+
     return (
       <div className="Welcome" style={{display: 'flex', flexDirection: 'column', backgroundColor: "#FFAF65"}}>
         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', backgroundColor: 'rgba(0,0,0,0.4)', alignItems: 'center'}}>
@@ -28,8 +32,11 @@ const Welcome = () => {
           <img src={Logo} alt="Logo" style={{width: "3%", height: "3%"}}/>
           <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: "10%"}}>
             {access_token !== "undefined" ? 
-              <h1>Hello Johan</h1> 
-              : 
+              <Button onClick={() => {_signOut()}} variant="outlined" style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: "80%", height: 38, fontWeight: 'bold', color: "white", borderColor: 'white', borderRadius: 50, borderWidth: 3}}>
+                <AccountCircle style={{fontSize: 40}} color="primary" />
+                <h4>Johan</h4>
+              </Button>
+                : 
               <Button href={signIn()} variant="outlined" style={{width: "80%", fontWeight: 'bold', color: "white", borderColor: 'white', borderRadius: 50, borderWidth: 3}}>
                 Sign In
               </Button>
@@ -55,10 +62,8 @@ const Welcome = () => {
           </div>
         </div>
         <div style={{backgroundColor: 'black', display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', height: 200}}>
-          <h2 style={{color: 'white'}}>Counter = {counter}</h2>
-          <Button variant="contained" color="primary" onClick={() => dispatch(increment(1))}>+</Button>
-          <Button variant="contained" color="primary" onClick={() => dispatch(decrement(1))}>-</Button>
-        </div>
+          <h2 style={{color:"white"}}>About and things like that !</h2>
+        </div> 
       </div>
     );
 };

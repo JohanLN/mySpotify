f81e080a3b4f938cd2379c8ea77c39047c7b5fea
Johan <johan.le-nagard@epitech.eu>
2021-03-24 09:38:39 +0100

    adding base theme using material ui

---
+++ my-spotify/src/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from '@material-ui/core';
+
+var white = {
+  500: '#ffffff',
+};
+
+var green = {
+    500: "#10CD0C",
+    A400: "#10CD0C"
+}
+
+export default createMuiTheme({
+    palette: {
+        primary: white,
+        secondary: green
+    },
+    typography: 'Raleway, Arial',
+})
